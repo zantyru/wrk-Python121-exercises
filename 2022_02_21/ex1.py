aa41181c4c5768122d1eba549ef85b6d744b650f
user <e@mail.mail>
2022-02-21 20:30:47 +0300

    Create ex1.py
    Пример наследования классов.

---
+++ 2022_02_21/ex1.py
@@ -0,0 +1,26 @@
+class Box:
+    def __init__(self, width, length, depth):
+        self.width = float(width)
+        self.length = float(length)
+        self.depth = float(depth)
+
+
+class MaterialBox(Box):
+    def __init__(self, width, length, depth, material="paper", color="red"):
+        super(MaterialBox, self).__init__(width, length, depth)
+        self.material = str(material)
+        self.color = str(color)
+
+if __name__ == '__main__':
+
+    box = Box(10, 50, 5)
+    material_box = MaterialBox(3, 3, 3, "metal", "chrome")
+
+    print(isinstance(box, Box))  # True
+    print(isinstance(box, MaterialBox))  # False
+    print(isinstance(material_box, Box))  # True
+    print(isinstance(material_box, MaterialBox))  # True
+
+    print("-----")
+    print(issubclass(Box, MaterialBox))  # False
+    print(issubclass(MaterialBox, Box))  # True
