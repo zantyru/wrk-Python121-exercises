e8168f87eac5f77f2e88a701b06d01db99d05829
Евгений Заонегин <zaonegin_s@mail.ru>
2022-03-31 01:22:12 +0300

    Исправлены опечатки.

--- 2022_03_30_Flask_naive_login/mainx.py
+++ 2022_03_30_Flask_naive_login/mainx.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("login.html")
+    return render_template("login.html", message=None, error=None)
 
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    return render_template("register.html", message=None, error=None)
 
 
 # Подсказка userlogin = request.args.get("lgn")
